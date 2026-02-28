7b3f1b24127c8811d1c66374b315e1c975f03275
Nikhil Chaudhary <144834519+aswathama18@users.noreply.github.com>
2026-02-28 14:49:39 +0000

    updated run

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 144335
+let accountEmail = "jayjama@gmail.com"
+var accountPassword = "18907"
+accountCity = "Noida"
+
+/*
+dont try to use var 
+becuase of issue in block and functional scope
+
+more often use let and var 
+*/
+
+
+accountEmail = "hchch@gmail.com"
+accountPassword= "232323"
+accountCity = "gurgaon"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict" // treat all the Js code as newer version 
+
+// alert(3 + 3) // we are using node js , not browser 
+
+console.log(3+3)
