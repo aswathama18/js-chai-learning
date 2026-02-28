422b1cad2b2001c8d44f922cc44fa41cbb144526
Nikhil Chaudhary <144834519+aswathama18@users.noreply.github.com>
2026-02-28 14:57:25 +0000

    update on 02_datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,25 @@
 // alert(3 + 3) // we are using node js , not browser 
 
 console.log(3+3)
+
+// code readability should be high 
+console.log("NIkhil")
+
+let name = "ajk"
+let age = 29
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigInt 
+// string => ""
+// bolean => false or true 
+// null => stand alone value 
+// undefined =>
+// // symbol => uniqueness 
+
+
+// object 
+
+console.log(typeof null);
+console.log(typeof undefined);
