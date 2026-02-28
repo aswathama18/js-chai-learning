c8c60291f7f20ee30d88d6713adf9d3b1ae552db
Nikhil Chaudhary <144834519+aswathama18@users.noreply.github.com>
2026-02-28 15:29:47 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+//let score= "90xxv"
+
+
+//console.log(typeof(score));
+//console.log(typeof score);
+
+/*
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+*/
+// "90" => 90
+//"90xxv" => NaN (not a number)
+// Boolean conversion => 1 for True ; 0 for false 
+
+let isLoggedIn = "hukum"
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//"" => false
+// "abc" => true
+
+
+let someNum = 98
+
+let stringNum = String(someNum)
+
+console.log(someNum);
+console.log(typeof stringNum);
