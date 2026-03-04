79154f87d007b2c772ababee5e36b7722d80b000
Nikhil Chaudhary <144834519+aswathama18@users.noreply.github.com>
2026-03-04 18:51:25 +0000

    02_arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -28,3 +28,11 @@ console.log(real_another_array);
 ]
 */
 
+console.log(Array.from("Hukum"));
+console.log(Array.from({name : "Hukum"})); // interesting because it gaves us an array of a name but empty 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
